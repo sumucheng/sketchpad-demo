f2bbef4af03c857d511c650da4c6c0fed7a5cf3d
kiki <772765729@qq.com>
2019-11-17 01:02:41 +0800

    update

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ function drawLine(x1, y1, x2, y2) {
 }
 
 let painting = false;
+let clearing = false;
 let last;
 
 let isTouchDevice = "ontouchstart" in document.documentElement;
@@ -28,23 +29,40 @@ if (isTouchDevice) {
   canvas.ontouchstart = e => {
     let x = e.touches[0].clientX;
     let y = e.touches[0].clientY;
-    last = [x, y];
+    if (clearing === true) {
+      console.log(clearing);
+      ctx.clearRect(x, y, lw, lw);
+    } else {
+      last = [x, y];
+    }
   };
   canvas.ontouchmove = e => {
     let x = e.touches[0].clientX;
     let y = e.touches[0].clientY;
-    drawLine(last[0], last[1], x, y);
-    last = [x, y];
+    if (clearing === true) {
+      ctx.clearRect(x, y, lw, lw);
+    } else {
+      drawLine(last[0], last[1], x, y);
+      last = [x, y];
+    }
   };
 } else {
   canvas.onmousedown = e => {
     painting = true;
-    last = [e.clientX, e.clientY];
+    if (clearing === true) {
+      ctx.clearRect(e.clientX, e.clientY, lw, lw);
+    } else {
+      last = [e.clientX, e.clientY];
+    }
   };
   canvas.onmousemove = e => {
-    if (painting === true) {
-      drawLine(last[0], last[1], e.clientX, e.clientY);
-      last = [e.clientX, e.clientY];
+    if (painting) {
+      if (clearing) {
+        ctx.clearRect(e.clientX, e.clientY, lw, lw);
+      } else {
+        drawLine(last[0], last[1], e.clientX, e.clientY);
+        last = [e.clientX, e.clientY];
+      }
     }
   };
   canvas.onmouseup = e => {
@@ -54,17 +72,23 @@ if (isTouchDevice) {
 
 document.querySelector(".lw-small").onclick = function() {
   lw = 5;
+  removeLW();
+  document.querySelector(".lw-border.small").classList.add("active");
 };
 document.querySelector(".lw-normal").onclick = function() {
   lw = 10;
+  removeLW();
+  document.querySelector(".lw-border.normal").classList.add("active");
 };
 document.querySelector(".lw-large").onclick = function() {
   lw = 20;
+  removeLW();
+  document.querySelector(".lw-border.large").classList.add("active");
 };
 
 document.querySelector(".lc-black").onclick = function() {
   lc = "black";
-  remove();
+  removeColor();
   document
     .querySelector(".lc-black")
     .querySelector(".circle")
@@ -72,27 +96,58 @@ document.querySelector(".lc-black").onclick = function() {
 };
 document.querySelector(".lc-red").onclick = function() {
   lc = "red";
-  remove();
+  removeColor();
   document
     .querySelector(".lc-red")
     .querySelector(".circle")
     .classList.add("active");
 };
+document.querySelector(".lc-yellow").onclick = function() {
+  lc = "yellow";
+  removeColor();
+  document
+    .querySelector(".lc-yellow")
+    .querySelector(".circle")
+    .classList.add("active");
+};
+document.querySelector(".lc-blue").onclick = function() {
+  lc = "blue";
+  removeColor();
+  document
+    .querySelector(".lc-blue")
+    .querySelector(".circle")
+    .classList.add("active");
+};
 document.querySelector(".lc-green").onclick = function() {
   lc = "green";
-  remove();
+  removeColor();
   document
     .querySelector(".lc-green")
     .querySelector(".circle")
     .classList.add("active");
 };
+
+document.querySelector(".pen").onclick = function() {
+  clearing = false;
+};
+document.querySelector(".eraser").onclick = function() {
+  clearing = true;
+};
 document.querySelector(".clear").onclick = function() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 };
 
-const remove = function() {
+const removeColor = function() {
   document
     .querySelector(".lc-green")
+    .querySelector(".circle")
+    .classList.remove("active");
+  document
+    .querySelector(".lc-blue")
+    .querySelector(".circle")
+    .classList.remove("active");
+  document
+    .querySelector(".lc-yellow")
     .querySelector(".circle")
     .classList.remove("active");
   document
@@ -103,4 +158,9 @@ const remove = function() {
     .querySelector(".lc-black")
     .querySelector(".circle")
     .classList.remove("active");
+};
+const removeLW = function() {
+  document.querySelector(".lw-border.small").classList.remove("active");
+  document.querySelector(".lw-border.normal").classList.remove("active");
+  document.querySelector(".lw-border.large").classList.remove("active");
 };
